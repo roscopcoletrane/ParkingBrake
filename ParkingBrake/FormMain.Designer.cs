6bc507b33ec811dedc1ef8dfa81c0223f580ab88
roscopcoletrane <kewlayde@gmail.com>
2013-03-08 23:53:11 -0500

    Initial commit

---
+++ ParkingBrake/FormMain.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ParkingBrake
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBoxMovies = new System.Windows.Forms.ListBox();
+			this.buttonMovie = new System.Windows.Forms.Button();
+			this.buttonTV = new System.Windows.Forms.Button();
+			this.buttonLoad = new System.Windows.Forms.Button();
+			this.textBoxFolderPath = new System.Windows.Forms.TextBox();
+			this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+			this.SuspendLayout();
+			// 
+			// listBoxMovies
+			// 
+			this.listBoxMovies.FormattingEnabled = true;
+			this.listBoxMovies.Location = new System.Drawing.Point(12, 41);
+			this.listBoxMovies.Name = "listBoxMovies";
+			this.listBoxMovies.Size = new System.Drawing.Size(250, 238);
+			this.listBoxMovies.TabIndex = 0;
+			// 
+			// buttonMovie
+			// 
+			this.buttonMovie.Location = new System.Drawing.Point(12, 285);
+			this.buttonMovie.Name = "buttonMovie";
+			this.buttonMovie.Size = new System.Drawing.Size(121, 23);
+			this.buttonMovie.TabIndex = 1;
+			this.buttonMovie.Text = "Movie";
+			this.buttonMovie.UseVisualStyleBackColor = true;
+			// 
+			// buttonTV
+			// 
+			this.buttonTV.Location = new System.Drawing.Point(141, 285);
+			this.buttonTV.Name = "buttonTV";
+			this.buttonTV.Size = new System.Drawing.Size(121, 23);
+			this.buttonTV.TabIndex = 2;
+			this.buttonTV.Text = "TV Show";
+			this.buttonTV.UseVisualStyleBackColor = true;
+			// 
+			// buttonLoad
+			// 
+			this.buttonLoad.Location = new System.Drawing.Point(12, 12);
+			this.buttonLoad.Name = "buttonLoad";
+			this.buttonLoad.Size = new System.Drawing.Size(39, 23);
+			this.buttonLoad.TabIndex = 3;
+			this.buttonLoad.Text = "Load";
+			this.buttonLoad.UseVisualStyleBackColor = true;
+			this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+			// 
+			// textBoxFolderPath
+			// 
+			this.textBoxFolderPath.Location = new System.Drawing.Point(57, 14);
+			this.textBoxFolderPath.Name = "textBoxFolderPath";
+			this.textBoxFolderPath.Size = new System.Drawing.Size(205, 20);
+			this.textBoxFolderPath.TabIndex = 4;
+			// 
+			// folderBrowserDialog
+			// 
+			this.folderBrowserDialog.SelectedPath = "F:\\TEST";
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(274, 314);
+			this.Controls.Add(this.textBoxFolderPath);
+			this.Controls.Add(this.buttonLoad);
+			this.Controls.Add(this.buttonTV);
+			this.Controls.Add(this.buttonMovie);
+			this.Controls.Add(this.listBoxMovies);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.Name = "FormMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ParkingBrake";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBoxMovies;
+		private System.Windows.Forms.Button buttonMovie;
+		private System.Windows.Forms.Button buttonTV;
+		private System.Windows.Forms.Button buttonLoad;
+		private System.Windows.Forms.TextBox textBoxFolderPath;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+	}
+}
+
---
+++ ParkingBrake/FormMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HandBrake_API;
+
+namespace ParkingBrake
+{
+	public partial class FormMain : Form
+	{
+		private BackgroundWorker SecondaryThread;
+		public List<Movie> movieList;
+
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonLoad_Click(object sender, EventArgs e)
+		{
+			if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				//Load Movies
+				InitMovieList(folderBrowserDialog.SelectedPath);
+			}
+		}
+
+		private void InitMovieList(string path)
+		{
+			//If list/ListBox is already populated, clear it out
+			listBoxMovies.DataSource = null;
+			listBoxMovies.DataSource = movieList;
+			listBoxMovies.Refresh();
+
+			textBoxFolderPath.Text = path;
+
+			movieList = new List<Movie>();
+
+			SecondaryThread = new BackgroundWorker();
+			SecondaryThread.DoWork += new DoWorkEventHandler(delegate { LoadMovies(path); });
+			SecondaryThread.WorkerReportsProgress = true;
+
+			if (!SecondaryThread.IsBusy)
+				SecondaryThread.RunWorkerAsync();
+
+			SecondaryThread.ProgressChanged += new ProgressChangedEventHandler(SecondaryThread_ProgressChanged);
+			SecondaryThread.RunWorkerCompleted += new RunWorkerCompletedEventHandler(SecondaryThread_RunWorkerCompleted);
+		}
+
+		private void LoadMovies(string path)
+		{
+			DirectoryInfo pathDirInfo = new DirectoryInfo(path);
+			DirectoryInfo[] movieFolders = null;
+
+			if (pathDirInfo.Exists == true)
+			{
+				movieFolders = pathDirInfo.GetDirectories();
+				int progress = 0;
+				int progMax = movieFolders.Length;
+
+				foreach (DirectoryInfo i in movieFolders)
+				{
+					Movie mov = new Movie(i.Name, path + "\\");
+
+					if (mov.GatherInfo())
+						movieList.Add(mov);
+
+					progress++;
+					SecondaryThread.ReportProgress(progress / progMax);
+				}
+			}
+		}
+
+		private void SecondaryThread_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			listBoxMovies.DataSource = null;
+			listBoxMovies.DataSource = movieList;
+			listBoxMovies.Refresh();
+		}
+
+		private void SecondaryThread_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			MessageBox.Show("Loading Complete!");
+
+			listBoxMovies.DataSource = null;
+			listBoxMovies.DataSource = movieList;
+			listBoxMovies.Refresh();
+		}
+	}
+}
